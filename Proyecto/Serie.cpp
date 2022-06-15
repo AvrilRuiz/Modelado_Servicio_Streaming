41fb674e09aff1af9f2a9c42c794014c3fd7f17a
Avril Ruiz <88905371+AvrilRuiz@users.noreply.github.com>
2022-06-14 22:20:04 -0500

    implementé la sobrecarga de operador
    implementé la sobrecarga de operador con función amiga, cambios en los métodos getEpisodio(), setEpisodio() y calculaCalPromedio()

--- Proyecto/Serie.cpp
+++ Proyecto/Serie.cpp
@@ -7,19 +7,25 @@
 
 #include "Serie.hpp"
 
+// constructores
 Serie::Serie() : Video() {
     cantidad = 0;
 }
 
-Serie::Serie(string _iD, string _titulo, int _duracion, string _genero, double _calificacionPromedio) : Video(_iD, _titulo, _duracion, _genero, _calificacionPromedio){
-    cantidad = 0;
+Serie::Serie(string _iD, string _titulo, int _duracion, string _genero, double _calificacionPromedio, int _cantidad) : Video(_iD, _titulo, _duracion, _genero, _calificacionPromedio){
+    cantidad = _cantidad;
+}
+
+// deconstructor
+Serie::~Serie(){
+    //cout << "Se destruyó un obejto Serie" << endl;
 }
 
 // métodos modificadores
 void Serie::setEpisodio(int _index, Episodio _episodio) {
     // validar que el _index sea >= 0 and  _index < cantidad
     // si no cumple el _index no se cambia
-    if ( _index >= 0 && _index < cantidad)
+    if ( _index >= 0 && _index <= cantidad)
         episodios[_index] = _episodio;
 }
 
@@ -32,7 +38,7 @@ Episodio Serie::getEpisodio(int _index) {
     // validar que el _index sea >= 0 and  _index < cantidad
     // si no cumple el _index no se regresa
     Episodio epi;
-    if ( _index >= 0 && _index < cantidad)
+    if ( _index >= 0 && _index <= cantidad)
         return episodios[_index];
     else
         return epi;
@@ -52,12 +58,22 @@ double Serie::calculaCalPromedio() {
         return sumatoria/cantidad;
     }
     else
-        return 0;
+        return -1;
 }
 
 string Serie::str() {
     string acumulador = "\n";
     for(int indice = 0; indice < cantidad; indice++)
-        acumulador = acumulador + episodios[indice].str() + '\n';
-    return "S " + iD + ',' + titulo + ',' + to_string(duracion) + ',' + genero + ',' + to_string(calificacionPromedio) + acumulador;
+        acumulador = acumulador +  to_string(indice) + ':' + episodios[indice].str() + '\n';
+    return "S" + iD + ',' + titulo + ',' + to_string(duracion) + ',' + genero + ',' + to_string(calificacionPromedio) + ',' + to_string(cantidad)  + acumulador;
 }
+
+// sobrecarga con función amiga
+ostream & operator << (ostream &out, const Serie &s) {
+    string acumulador = "\n";
+    for(int indice = 0; indice < s.cantidad; indice++)
+        acumulador = acumulador +  to_string(indice) + ':' + s.episodios[indice].str() + '\n';
+    out << 'S' << s.iD << ',' << s.titulo << ',' << s.duracion << ',' << s.genero << ',' << s.calificacionPromedio << ',' << s.cantidad << acumulador << endl;
+    return out;
+}
+
