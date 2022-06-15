76ac3c908fb4b6f3d317249baff5c7a02eab30e1
Avril Ruiz <88905371+AvrilRuiz@users.noreply.github.com>
2022-06-14 22:12:56 -0500

    calificacion double y str const

--- Proyecto/Episodio.cpp
+++ Proyecto/Episodio.cpp
@@ -8,41 +8,46 @@
 #include "Episodio.hpp"
 
 // Métodos constructores - sirven para inicializar los atributos
-    Episodio::Episodio(){
-        titulo = "titulo";
-        temporada = 0;
-        calificacion = 0;
-    }
+Episodio::Episodio(){
+    titulo = "titulo";
+    temporada = 0;
+    calificacion = 0;
+}
 
-    Episodio::Episodio(string _tituloEpisodio, int _temporada, int _calificacion){
-        titulo = _tituloEpisodio;
-        temporada = _temporada;
-        calificacion = _calificacion;
-    }
+Episodio::Episodio(string _tituloEpisodio, int _temporada, double _calificacion){
+    titulo = _tituloEpisodio;
+    temporada = _temporada;
+    calificacion = _calificacion;
+}
 
-    //Métodos modificadores
-    void Episodio::setTitulo(string _titulo){
-        titulo= _titulo;
-    }
-    void Episodio::setTemporada(int _temporada){
-        temporada = _temporada;
-    }
-    void Episodio::setCalificacion(int _calificacion){
-        calificacion = _calificacion;
-    }
+// método deconstructor
+Episodio::~Episodio(){
+   // cout << "Se destruyó un obejto Episodio" << endl;
+}
 
-    //Métodos de acceso
-    string Episodio::getTitulo(){
-        return titulo;
-    }
-    int Episodio::getTemporada(){
-        return temporada;
-    }
-    int Episodio::getCalificacion(){
-        return calificacion;
-    }
-    
-  //Otro Métodos
-    string Episodio::str(){
-        return "E " + titulo + ',' + to_string(temporada) + ',' + to_string(calificacion);
-    }
+//Métodos modificadores
+void Episodio::setTitulo(string _titulo){
+    titulo = _titulo;
+}
+void Episodio::setTemporada(int _temporada){
+    temporada = _temporada;
+}
+void Episodio::setCalificacion(double _calificacion){
+    calificacion = _calificacion;
+}
+
+//Métodos de acceso
+string Episodio::getTitulo(){
+    return titulo;
+}
+int Episodio::getTemporada(){
+    return temporada;
+}
+double Episodio::getCalificacion(){
+    return calificacion;
+}
+
+//Otro Métodos
+string Episodio::str() const {
+    return "E" + titulo + ',' + to_string(temporada) + ',' + to_string(calificacion);
+}
