2b6aa968164acc7c082a2df318a3b2403d917ab5
Avril Ruiz <88905371+AvrilRuiz@users.noreply.github.com>
2022-06-14 22:08:38 -0500

    Implementación de la sobrecarga con función amiga

--- Proyecto/Pelicula.cpp
+++ Proyecto/Pelicula.cpp
@@ -16,6 +16,11 @@ Pelicula::Pelicula(string _iD, string _titulo, int _duracion, string _genero, do
     oscares = _oscares;
 }
 
+// método deconstructor
+Pelicula::~Pelicula(){
+   // cout << "Se destruyó un obejto Película" << endl;
+}
+
 // método modificador
 void Pelicula::setOscares(int _oscares) {
     oscares = _oscares;
@@ -28,6 +33,11 @@ int Pelicula::getOscares() {
 
 // otro método
 string Pelicula::str() {
-    return "P " + iD + ',' + titulo + ',' + to_string(duracion) + ',' + genero + ',' + to_string(calificacionPromedio) + ',' + to_string(oscares);
+    return "P" + iD + ',' + titulo + ',' + to_string(duracion) + ',' + genero + ',' + to_string(calificacionPromedio) + ',' + to_string(oscares);
 }
 
+// sobrecarga con función amiga 
+ostream & operator << (ostream &out, const Pelicula &p) {
+    out << "P" << p.iD << ',' << p.titulo << ',' << p.duracion << ',' << p.genero << ',' << p.calificacionPromedio << ',' << p.oscares << endl;
+    return out;
+}
